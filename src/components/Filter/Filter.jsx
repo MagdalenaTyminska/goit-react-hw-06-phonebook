a5ba708b15576b2638df92d08b5d446b24583afc
Magdalena Bonk <106923695+MagdaBonk@users.noreply.github.com>
2023-07-19 22:38:32 +0200

    .

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,18 +1,7 @@
 import React from 'react';
 import css from './Filter.module.css';
-import { useDispatch, useSelector } from 'react-redux';
-import { filterContact } from '../../redux/filterSlice';
-import { getFilter } from 'redux/selectors';
 
-const Filter = () => {
-  const filter = useSelector(getFilter);
-  const dispatch = useDispatch();
-
-  const handleSearch = event => {
-    const { value } = event.target;
-    dispatch(filterContact(value));
-  };
-
+const Filter = ({ filter, handleSearch }) => {
   return (
     <>
       <p className={css.filterTitle}>Find contacts by name</p>
@@ -27,22 +16,3 @@ const Filter = () => {
 };
 
 export default Filter;
-
-// import React from 'react';
-// import css from './Filter.module.css';
-
-// const Filter = ({ filter, handleSearch }) => {
-//   return (
-//     <>
-//       <p className={css.filterTitle}>Find contacts by name</p>
-//       <input
-//         className={css.filterInput}
-//         type="text"
-//         value={filter}
-//         onChange={handleSearch}
-//       />
-//     </>
-//   );
-// };
-
-// export default Filter;
